d52eba2e722d31297f4bf220e79683eddd70a519
shivakumar901072 <shivakumar901072@gmail.com>
2020-11-01 12:27:34 +0530

    commit changes newton school

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+// adding something to commit change//
 
 ReactDOM.render(<App />, document.getElementById("root"));
