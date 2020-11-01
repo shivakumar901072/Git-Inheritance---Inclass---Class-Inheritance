562400c786a5973affa9b42d9dd1a8e61bf28888
shivakumar901072 <shivakumar901072@gmail.com>
2020-11-01 13:22:10 +0530

    git first problem

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,27 @@
 // declare class
+/*Acceptance Criteria
+
+Create a base class called "Shape" with methods drawShape, calculateArea and color property that is initialised with a color.
+
+In another module make a class called "Circle" that extends the "Shape" class and overrides the calculateArea method.
+
+Write code for Shape class in shape.js and Circle in circle.js
+
+Export the class from each file.*/
 
 // export class using module.exports
+
+class Shape {
+    constructor() {
+        this.color = "red";
+    }
+    drawShape() {
+        console.log("Drawing a Shape");
+
+    }
+    calculateArea() {
+        console.log("Dont know area of unknown shape");
+        return 0;
+    }
+}
+module.exports = Shape;
